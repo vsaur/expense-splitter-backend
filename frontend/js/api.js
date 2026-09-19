b47bbcf7a1099b8d5f10ded2ac95511e31a4a6d6
saurav <134272922+vsaur@users.noreply.github.com>
2026-09-19 17:22:38 +0530

    Update api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080/api'
+const BASE_URL = 'https://expense-splitter-backend-production-9309.up.railway.app/api'
 
 const getToken = () => localStorage.getItem('token')
 
